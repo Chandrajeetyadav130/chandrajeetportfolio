49adbf26b2f91980a3ee764056690d1f6e270aea
Chandrajeetyadav130 <95475093+Chandrajeetyadav130@users.noreply.github.com>
2025-05-14 14:57:37 +0530

    Update About.js

--- pages/About.js
+++ pages/About.js
@@ -7,7 +7,7 @@ const About = () => {
                 <h2>Hi , my name is</h2>
                 <h2>Chandrajeet Yadav</h2>
                 <h2>I am Reactjs developer</h2>
-                <div className={styles.contents}>Software Engineer with over 1.5+ Years of Professional Experience in Software Development, Specializing in Html, JavaScript, ReactJs, NextJs, Redux ,Tailwind css,Bootstrap,Material Ui css etc. Committed to delivering high-quality results and make a meaningful contribution and achieve great things.</div>
+                <div className={styles.contents}>Software Engineer with over 2.4 Years of Professional Experience in Software Development, Specializing in Html, JavaScript, ReactJs, NextJs, Redux ,Tailwind css,Bootstrap,Material Ui css etc. Committed to delivering high-quality results and make a meaningful contribution and achieve great things.</div>
             </section>
     )
 }
